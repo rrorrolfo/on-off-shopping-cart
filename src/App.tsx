f27ac438e1bfe8d3f02c8dab24714bd0d9091ef3
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-12 01:28:21 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg"
+import { Counter } from "./features/counter/Counter"
+import ShoppingCart from "./features/shoppingCart"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <ShoppingCart />
+    </div>
+  )
+}
+
+export default App
