f709295d53cd3a1cb4582469ec95cc11670bdae6
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-12 09:56:56 +0200

    Created and styled SearchInput component

---
+++ src/features/shoppingCart/components/searchInput/index.tsx
@@ -0,0 +1,24 @@
+import "./searchInput.scss"
+
+interface ISearchInputProps extends React.HTMLAttributes<HTMLInputElement> {
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const SearchInput = ({ value, onChange, ...props }: ISearchInputProps) => {
+  return (
+    <input
+      type="text"
+      id="product-search-input"
+      data-testid="product-search-input"
+      name="product-search-input"
+      onChange={onChange}
+      value={value}
+      placeholder="Name"
+      className="search-input"
+      {...props}
+    />
+  )
+}
+
+export default SearchInput
