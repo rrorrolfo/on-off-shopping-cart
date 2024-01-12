10531935f65d058ea537e9dc69e3ac837a2499e2
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-12 14:51:37 +0200

    Created and styled Button component

---
+++ src/features/shoppingCart/components/button/index.tsx
@@ -0,0 +1,23 @@
+import "./button.scss"
+
+export interface IButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+}
+
+const Button = ({ children = "Button", onClick, ...props }: IButtonProps) => {
+  return (
+    <button
+      role="button"
+      type="button"
+      aria-label="add-button"
+      className="add-button"
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
