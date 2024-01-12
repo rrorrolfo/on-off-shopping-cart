676db3064dfba1501ff55e5fa98cc327855549e5
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-12 14:07:59 +0200

    Added SelecInput component to ShoppingCart component

--- src/features/shoppingCart/index.tsx
+++ src/features/shoppingCart/index.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react"
 
 import SearchInput from "./components/searchInput"
+import SelectInput from "./components/selectInput"
 import "./shoppingCart.scss"
 
 const ShoppingCart = () => {
   const [searchValue, setSearchValue] = useState("")
+  const [selectedShop, setShop] = useState("")
   return (
     <div className="shopping-cart">
       <span className="shopping-cart__title">Add to cart:</span>
       <SearchInput
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <SelectInput
+        options={[]}
+        value={selectedShop}
+        onChange={(e) => setShop(e.target.value)}
       />
     </div>
   )
