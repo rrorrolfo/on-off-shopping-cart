e51ce7e85e9d15e511600bf6fcce87e092e089f0
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-14 19:22:43 +0200

    Added REMOVE_PRODUCT case to delete a product from the shopping cart

--- src/store/reducers/shoppingCart.ts
+++ src/store/reducers/shoppingCart.ts
@@ -17,6 +17,15 @@ const shoppingCart = (
   switch (action.type) {
     case ADD_PRODUCT:
       return { ...state, products: [...state.products, action.payload] }
+    case REMOVE_PRODUCT:
+      return {
+        ...state,
+        products: [
+          ...state.products.filter(
+            (product) => product.id !== action.payload.id,
+          ),
+        ],
+      }
     default:
       return state
   }
