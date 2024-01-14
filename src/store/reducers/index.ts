02e25a4115245bec1580bc507d1d25b42a7ff170
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-14 03:36:40 +0200

    Added shopping cart reducer to rootReducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
+import shoppingCart from "./shoppingCart"
 import shops from "./shops"
 
 const rootReducer = combineReducers({
   shops,
+  shoppingCart,
 })
 
 export default rootReducer
