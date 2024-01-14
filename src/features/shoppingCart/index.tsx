dab9031adf9f232e7ad147ed311212a31ca534a9
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-14 02:35:16 +0200

    Added functionality to fetch and display shops list inside ShoppingCart container

--- src/features/shoppingCart/index.tsx
+++ src/features/shoppingCart/index.tsx
@@ -1,13 +1,24 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import Input from "./components/Input"
 import SelectInput from "./components/selectInput"
 import Button from "./components/button"
 import "./shoppingCart.sass"
 
+import { selectShopsList } from "../../store/reducers/shops"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+import { fetchShops } from "../../store/actions/shops"
+
 const ShoppingCart = () => {
+  const dispatch = useAppDispatch()
+
   const [searchValue, setSearchValue] = useState("")
   const [selectedShop, setShop] = useState("")
+  const shopsList = useAppSelector(selectShopsList)
+
+  useEffect(() => {
+    dispatch(fetchShops())
+  }, [])
   return (
     <div className="shopping-cart">
       <span className="shopping-cart__title">Add to cart:</span>
@@ -16,7 +27,7 @@ const ShoppingCart = () => {
         onChange={(e) => setSearchValue(e.target.value)}
       />
       <SelectInput
-        options={[]}
+        options={shopsList}
         value={selectedShop}
         onChange={(e) => setShop(e.target.value)}
       />
