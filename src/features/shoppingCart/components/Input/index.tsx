0b0b11581057a73a67430e0046582e306133f14f
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-14 23:41:30 +0200

    Added functionality for input component to display an error rmessage

--- src/features/shoppingCart/components/Input/index.tsx
+++ src/features/shoppingCart/components/Input/index.tsx
@@ -3,21 +3,34 @@ import "./Input.sass"
 interface IInputProps extends React.HTMLAttributes<HTMLInputElement> {
   value: string
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  invalid?: boolean
+  showError?: boolean
+  errorMessage?: string
 }
 
-const Input = ({ value, onChange, ...props }: IInputProps) => {
+const Input = ({
+  value,
+  onChange,
+  invalid = false,
+  showError = false,
+  errorMessage = "",
+  ...props
+}: IInputProps) => {
   return (
-    <input
-      type="text"
-      id="product-search-input"
-      data-testid="product-search-input"
-      name="product-search-input"
-      onChange={onChange}
-      value={value}
-      placeholder="Name"
-      className="search-input"
-      {...props}
-    />
+    <div className="input--container">
+      <input
+        type="text"
+        id="product-search-input"
+        data-testid="product-search-input"
+        name="product-search-input"
+        onChange={onChange}
+        value={value}
+        placeholder="Name"
+        className={`input ${invalid ? "invalid" : ""}`}
+        {...props}
+      />
+      {showError && <span className="error-message">{errorMessage}</span>}
+    </div>
   )
 }
 
