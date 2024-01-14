a672abe1882c0e1321926a01016ce3bc59989351
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-15 00:19:29 +0200

    Added functionality to display error message to Select component

--- src/features/shoppingCart/components/selectInput/index.tsx
+++ src/features/shoppingCart/components/selectInput/index.tsx
@@ -5,26 +5,41 @@ interface ISelecInputProps extends React.HTMLAttributes<HTMLSelectElement> {
   options: ShopType[]
   value: string
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
+  invalid?: boolean
+  showError?: boolean
+  errorMessage?: string
 }
 
-const SelectInput = ({ options, value, onChange }: ISelecInputProps) => {
+const SelectInput = ({
+  options,
+  value,
+  onChange,
+  invalid = false,
+  showError = false,
+  errorMessage = "",
+  ...props
+}: ISelecInputProps) => {
   return (
-    <select
-      name="shops-select"
-      id="shops-select"
-      value={value}
-      onChange={onChange}
-      role="select"
-      aria-label="shops-select"
-      className="shops-select"
-    >
-      <option value="">Select Shop</option>
-      {options.map(({ id, name }) => (
-        <option value={id} key={id} role="option">
-          {name}
-        </option>
-      ))}
-    </select>
+    <div className="select--container">
+      <select
+        name="shops-select"
+        id="shops-select"
+        value={value}
+        onChange={onChange}
+        role="select"
+        aria-label="shops-select"
+        className={`shops-select ${invalid ? "invalid" : ""}`}
+        {...props}
+      >
+        <option value="">Select Shop</option>
+        {options.map(({ id, name }) => (
+          <option value={id} key={id} role="option">
+            {name}
+          </option>
+        ))}
+      </select>
+      {showError && <span className="error-message">{errorMessage}</span>}
+    </div>
   )
 }
 
