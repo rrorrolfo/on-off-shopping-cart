5c066ebb2fec774628097b99e6b05e7a6c01f25f
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-14 23:42:41 +0200

    Added test to validate error message functionality for Input component

--- src/features/shoppingCart/components/Input/Input.test.tsx
+++ src/features/shoppingCart/components/Input/Input.test.tsx
@@ -1,17 +1,45 @@
 import { render, screen } from "@testing-library/react"
-import SearchInput from "."
+import Input from "."
 
-test("Search Input is rendered", () => {
-  render(<SearchInput value="" onChange={() => {}} />)
+test("Input is rendered", () => {
+  render(<Input value="" onChange={() => {}} />)
   const inputElement = screen.getByPlaceholderText(/Name/i)
   expect(inputElement).toBeInTheDocument()
 })
 
-test("Search Input should display the new value", () => {
-  const { rerender } = render(<SearchInput value="" onChange={() => {}} />)
+test("Input should display the new value", () => {
+  const { rerender } = render(<Input value="" onChange={() => {}} />)
   const inputElement = screen.getByPlaceholderText(/Name/i)
   expect(inputElement).toHaveValue("")
 
-  rerender(<SearchInput value="Milk" onChange={() => {}} />)
+  rerender(<Input value="Milk" onChange={() => {}} />)
   expect(inputElement).toHaveValue("Milk")
+})
+
+test("Input should NOT show error message and error styling when it does not have an error", () => {
+  render(<Input value="" onChange={() => {}} />)
+  const inputElement = screen.getByPlaceholderText(/Name/i)
+  const messageElement = screen.queryByText(/error message/i)
+
+  expect(inputElement).toBeInTheDocument()
+  expect(messageElement).not.toBeInTheDocument()
+  expect(inputElement.getAttribute("class")).not.toMatch(/invalid/gi)
+})
+
+test("Input should show correct style and message when it has an error", () => {
+  render(
+    <Input
+      value=""
+      onChange={() => {}}
+      invalid
+      showError
+      errorMessage="Error message"
+    />,
+  )
+  const inputElement = screen.getByPlaceholderText(/Name/i)
+  const messageElement = screen.getByText(/error message/i)
+
+  expect(inputElement).toBeInTheDocument()
+  expect(messageElement).toBeInTheDocument()
+  expect(inputElement.getAttribute("class")).toMatch(/invalid/gi)
 })
