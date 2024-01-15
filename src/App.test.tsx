802833e5415bb163af8b60e86bddcb4d10c89040
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-15 10:37:03 +0200

    Updated app test to make sure App component is renderded

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,15 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
 import App from "./App"
 
-test("renders learn react link", () => {
-  const { getByText } = render(
+test("Renders App component", () => {
+  render(
     <Provider store={store}>
       <App />
     </Provider>,
   )
 
-  // expect(getByText(/learn/i)).toBeInTheDocument()
+  const appElement = screen.getByTestId("app-container")
+  expect(appElement).toBeInTheDocument()
 })
